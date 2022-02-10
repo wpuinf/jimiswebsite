47dddd5cabc96d838f2da0f2dcdae8cff39d931a
NinoDS <contact@jimi.sexy>
2022-02-10 16:30:41 +0100

    Changed hangman game

--- src/informatik/hangman/script.js
+++ src/informatik/hangman/script.js
@@ -3,10 +3,12 @@ function init() {
         method: 'GET'
     }).then(async request => {
         const response = await request.json();
-        localStorage.setItem('word', response[0].toLowerCase());
+        const word = toUtf8(response[0]).toLowerCase();
+        localStorage.setItem('word', word);
         localStorage.setItem('wrongGuesses', JSON.stringify([]));
+        console.log(word);
         let uscores = "";
-        for (let i = 0; i < response[0].length; i++) {
+        for (let i = 0; i < word.length; i++) {
             uscores += "_";
         }
         document.getElementById('word').innerHTML = uscores;
@@ -16,6 +18,8 @@ function init() {
 function guess() {
     const guess = document.getElementById('guessIn').value.toLowerCase();
     let wrongGuesses = JSON.parse(localStorage.getItem('wrongGuesses'));
+    let word = localStorage.getItem('word');
+    let uscores = document.getElementById('word').innerHTML;
     if (guess.length != 1) {
         clearGuess();
         document.getElementById('errors').innerHTML = "Bitte EINEN Buchstabe eingeben!";
@@ -26,13 +30,11 @@ function guess() {
         document.getElementById('errors').innerHTML = "Bitte Buchstabe eingeben!";
         return;
     }
-    if (wrongGuesses.includes(guess)) {
+    if (wrongGuesses.includes(guess) || uscores.includes(guess)) {
         clearGuess();
-        document.getElementById('errors').innerHTML = "Du hast diesen Buchstaben bereits versucht zu raten!";
+        document.getElementById('errors').innerHTML = "Du hast diesen Buchstaben bereits geraten!";
         return;
     }
-    let word = localStorage.getItem('word');
-    let uscores = document.getElementById('word').innerHTML;
 
     if (word.toLowerCase().includes(guess)) {
         for (let i = 0; i < word.length; i++) {
@@ -58,6 +60,8 @@ function guess() {
         };
         clearGuess();
     }
+
+    document.getElementById('currentHangman').innerHTML = `<img src="./hangmen/${wrongGuesses.length}.png" alt="Hangman">`;
 };
 
 function clearGuess() {
@@ -65,4 +69,11 @@ function clearGuess() {
 }
 function clearErrors() {
     document.getElementById('errors').innerHTML = "";
+}
+
+function toUtf8(string) {
+    document.getElementById('showWord').innerHTML = string;
+    string = document.getElementById('showWord').innerHTML;
+    document.getElementById('showWord').innerHTML = "";
+    return string;
 }
